71ce9685b003389a363798cf48961afec09e4bee
Benjamin Scher Purcell <ben@codigo.co>
2015-07-24 19:42:28 -0500

    working on attach

--- 9p.c
+++ 9p.c
@@ -21,22 +21,22 @@ init9p(int m)
 {
 	if((msize = _9pversion(srvfd, m) <= 0))
 		errx(1, "Bad msize");
-	tbuf = erealloc(tbuf, msize);
-	rbuf = erealloc(rbuf, msize);
+	tbuf = emalloc(msize);
+	rbuf = emalloc(msize);
 	if((rootfid = _9pattach(srvfd, 0, (uint16_t)~0)) == NULL)
 		errx(1, "Could not attach.");
 }
 
 int
-do9p(Fcall *t, Fcall *r)
+do9p(Fcall *t, Fcall *r, char *tb, char *rb)
 {
 	int	n;
 
-	n = convS2M(t, tbuf, msize);
-	write(srvfd, tbuf, n);
-	if((n = read9pmsg(srvfd, rbuf, msize)) == -1)
+	n = convS2M(t, tb, msize);
+	write(srvfd, tb, n);
+	if((n = read9pmsg(srvfd, rb, msize)) == -1)
 		errx(1, "Bad 9p read.");
-	convM2S(rbuf, n, r);
+	convM2S(rb, n, r);
 	if(r->type == Rerror)
 		return RERROR;
 	if(r->type != t->type+1)
@@ -48,15 +48,16 @@ int
 _9pversion(int fd, uint32_t m)
 {
 	Fcall	tver, rver;
+	char	*t, *r;
 
-	tbuf = emalloc(m);
-	rbuf = emalloc(m);
+	t = emalloc(m);
+	r = emalloc(m);
 	tver.type = Tversion;
 	tver.tag = (ushort)~0;
 	tver.msize = m;
 	tver.version = VERSION9P;
 
-	if(do9p(&tver, &rver) != 0)
+	if(do9p(&tver, &rver, t, r) != 0)
 		errx(1, "Could not establish version.");
 	return rver.msize;
 }
@@ -64,5 +65,12 @@ _9pversion(int fd, uint32_t m)
 FFid*
 _9pattach(int fd, uint16_t fid, uint16_t afid)
 {
-	return NULL;
+	Fcall tattach, rattach;
+
+	tattach.type = Tattach;
+	tattach.tag = 0;
+	tattach.fid = fid;
+	tattach.afid = afid;
+	tattach.msize = msize;
+	
 }	

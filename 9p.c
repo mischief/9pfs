ad21494559e18866e9bfe25da8dd4089a2a27537
Benjamin Scher Purcell <ben@codigo.co>
2015-07-28 19:06:44 -0500

    getattr finished?

--- 9p.c
+++ 9p.c
@@ -177,14 +177,20 @@ _9pstat(FFid *f, struct stat *s)
 	}
 	s->st_dev = d->dev;
 	s->st_ino = d->qid.path;
-	s->st_mode = d->mode;
+	s->st_mode = d->mode & 0777;
+	if(d->mode & DMDIR)
+		s->st_mode |= S_IFDIR;
+	else
+		s->st_mode |= S_IFREG;
 	s->st_nlink = d->mode & DMDIR ? r.nstat + 1 : 1;
 	s->st_uid = (p = getpwnam(d->uid)) == NULL ? 0 : p->pw_uid;
 	s->st_gid = (g = getgrnam(d->gid)) == NULL ? 0 : g->gr_gid;
 	s->st_size = d->length;
+	s->st_blksize = msize - IOHDRSZ;
+	s->st_blocks = (d->length / (msize - IOHDRSZ)) + 1;
 	s->st_atime = d->atime;
 	s->st_mtime = s->st_ctime = d->mtime;
-	/* what about the strings? */
+	s->st_rdev = 0;
 	free(d);
 	return 0;
 }
